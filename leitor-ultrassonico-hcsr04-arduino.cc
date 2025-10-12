1fb8f2198f212e132aa3c1d2dabafcfa9fcc4829
Fábio Henrique <fabio.h591@gmail.com>
2025-10-12 19:58:55 -0400

    feat: adicionar código do leitor ultrassônico HCSR04 em C++

---
+++ leitor-ultrassonico-hcsr04-arduino.cc
@@ -0,0 +1,39 @@
+// C++ code
+//
+float cm, duracao; // comprimento da onda
+
+int pinoTRIG = 11;
+int pinoECHO = 10;
+int pinoLED = 13;
+void setup()
+{
+  pinMode(      
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}// C++ code
+//
+float cm, duracao; // comprimento da onda
+
+int pinoTRIG = 11;
+int pinoECHO = 10;
+int pinoLED = 13;
+void setup()
+{
+  pinMode(      
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
